e5bcf999e9577f4455df15aa29dccf1be80e516d
leonardomoraes <leonardo.moraes@arctouch.com>
2019-06-04 17:41:32 -0300

    Imports libraries

--- app/src/main/java/com/arctouch/giphy/MainActivity.kt
+++ app/src/main/java/com/arctouch/giphy/MainActivity.kt
@@ -2,11 +2,14 @@ package com.arctouch.giphy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        exampleTextView.text = getString(R.string.app_name)
     }
 }
